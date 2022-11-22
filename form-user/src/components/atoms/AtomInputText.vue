30f59e6cc99755cfdf7729e68e8c1dcd6fd9b4ec
Michael SN <michael.dev.nascimento@gmail.com>
2022-11-22 18:46:19 -0300

    feat[InputText]: emit event

--- form-user/src/components/atoms/AtomInputText.vue
+++ form-user/src/components/atoms/AtomInputText.vue
@@ -1,13 +1,17 @@
 <template>
   <input
     class="rounded-lg border border-gray-400 px-4 py-3 transition-colors focus:border focus:border-violet-500 focus:outline-0"
-    :type="typeInput" :name="nameInput" :id="idInput" :required="required" />
+    :type="typeInput" :name="nameInput" :id="idInput" :required="required" @input="updateValue" />
 </template>
 
 <script>
 export default {
   name: "AtomInputText",
-  props: ['typeInput', 'idInput', 'nameInput', 'required']
-
+  props: ['typeInput', 'idInput', 'nameInput', 'required', 'value'],
+  methods: {
+    updateValue(event) {
+      this.$emit('update:value', event.target.value)
+    }
+  },
 }
 </script>
