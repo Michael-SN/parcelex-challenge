45e219c1902241a5354bea6fdbfa496396b47315
Michael SN <michael.dev.nascimento@gmail.com>
2022-11-22 11:46:14 -0300

    refact[AtomButton]: slot added for dynamic inside content

--- form-user/src/components/AtomButton.vue
+++ form-user/src/components/AtomButton.vue
@@ -1,12 +1,11 @@
 <template>
   <button type="submit" class="w-full py-3 rounded-lg text-white transition-colors bg-violet-600  hover:bg-violet-700">
-    {{ btnText }}
+    <slot></slot>
   </button>
 </template>
 
 <script>
 export default {
   name: "AtomButton",
-  props: ['btnText']
 }
 </script>
