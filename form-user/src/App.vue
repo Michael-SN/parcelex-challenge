6e68d9a30240e20d5dd35630316311f30b9a7ca4
Michael SN <michael.dev.nascimento@gmail.com>
2022-11-22 12:12:16 -0300

    refact[Components]: Applying the atomic design

--- form-user/src/App.vue
+++ form-user/src/App.vue
@@ -1,43 +1,15 @@
 <template>
   <div class="px-8">
-    <section class="mx-auto mt-12 w-full sm:w-[600px] rounded-xl border bg-white border-gray-100 p-8 shadow-lg">
-
-      <AtomTitle tag="h2" content="Create your account" />
-
-      <OrganismForm form-action="/endpoint" id="formUser" method="POST">
-
-        <MoleculeFormGroup legend="Name" for-input="txtName">
-          <AtomInputText type-input="text" name-input="user_name" id="txtName" required="true" />
-        </MoleculeFormGroup>
-
-        <MoleculeFormGroup legend="E-mail" for-input="txtEmail">
-          <AtomInputText type-input="email" name-input="user_email" id="txtEmail" required="true" />
-        </MoleculeFormGroup>
-
-        <MoleculeFormGroup legend="Telephone" for-input="txtTelephone">
-          <AtomInputText type-input="text" name-input="user_telephone" id="txtTelephone" required="true" />
-        </MoleculeFormGroup>
-
-        <MoleculeFormGroup legend="Age" for-input="dataUserBirth">
-          <AtomInputText type-input="text" name-input="user_birthdate" id="dataUserBirth" required="false" />
-        </MoleculeFormGroup>
-        <AtomButton btn-text="Submit" />
-
-      </OrganismForm>
-
-    </section>
+    <TemplateFormUser />
   </div>
 
 </template>
 
 <script>
-import MoleculeFormGroup from './components/MoleculeFormGroup.vue';
-import AtomButton from './components/AtomButton.vue';
-import AtomInputText from './components/AtomInputText.vue';
-import AtomTitle from './components/AtomTitle.vue';
-import OrganismForm from './components/OrganismForm.vue';
+import TemplateFormUser from './components/templates/TemplateFormUser.vue';
+
 
 export default {
-  components: { MoleculeFormGroup, AtomButton, AtomInputText, AtomTitle, OrganismForm }
+  components: { TemplateFormUser }
 }
 </script>
---
+++ form-user/src/components/templates/TemplateFormUser.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="mx-auto mt-12 w-full sm:w-[600px] rounded-xl border bg-white border-gray-100 p-8 shadow-lg">
+    <AtomTitle tag="h2" content="Create your account" />
+
+    <OrganismForm form-action="/endpoint" id="formUser" method="POST">
+
+      <MoleculeFormGroup legend="Name" for-input="txtName">
+        <AtomInputText type-input="text" name-input="user_name" id="txtName" required="true" />
+      </MoleculeFormGroup>
+
+      <MoleculeFormGroup legend="E-mail" for-input="txtEmail">
+        <AtomInputText type-input="email" name-input="user_email" id="txtEmail" required="true" />
+      </MoleculeFormGroup>
+
+      <MoleculeFormGroup legend="Telephone" for-input="txtTelephone">
+        <AtomInputText type-input="text" name-input="user_telephone" id="txtTelephone" required="true" />
+      </MoleculeFormGroup>
+
+      <MoleculeFormGroup legend="Age" for-input="dataUserBirth">
+        <AtomInputText type-input="text" name-input="user_birthdate" id="dataUserBirth" required="false" />
+      </MoleculeFormGroup>
+      <AtomButton>
+        Submit
+      </AtomButton>
+
+    </OrganismForm>
+  </section>
+</template>
+
+<script>
+import MoleculeFormGroup from '@/molecules/MoleculeFormGroup.vue';
+import AtomButton from '@/atoms/AtomButton.vue';
+import AtomInputText from '@/atoms/AtomInputText.vue';
+import AtomTitle from '@/atoms/AtomTitle.vue';
+import OrganismForm from '@/organisms/OrganismForm.vue';
+
+export default {
+  name: 'TemplateFormUser',
+  components: { MoleculeFormGroup, AtomButton, AtomInputText, AtomTitle, OrganismForm }
+}
+</script>
