c086eb06c799eb083049f5e0afdefe2bc2a3b2d9
Michael SN <michael.dev.nascimento@gmail.com>
2022-11-22 17:09:43 -0300

    refact[MoleculeFormGroup]: conditioning the props inside the component

--- form-user/src/components/molecules/MoleculeFormGroup.vue
+++ form-user/src/components/molecules/MoleculeFormGroup.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col mb-4">
-    <label :for="forInput" class="mb-3 font-semibold tracking-wide">{{ legend ? legend + ':' : '' }}</label>
+    <label v-if="legend" :for="forInput" class="mb-3 font-semibold tracking-wide">{{ legend }}</label>
     <slot></slot>
   </div>
 </template>
