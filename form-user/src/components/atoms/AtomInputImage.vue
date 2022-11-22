201d9e5a4e669270f82691960001a8c1df4680af
Michael SN <michael.dev.nascimento@gmail.com>
2022-11-22 17:08:46 -0300

    feat[InputImage]: input file and image preview added

--- form-user/src/components/atoms/AtomInputImage.vue
+++ form-user/src/components/atoms/AtomInputImage.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-    <input type="file" name="use_img" id="">
+  <div class="relative mx-auto w-[200px] overflow-hidden border-[2px] border-violet-600 rounded-full">
+    <figure class="min-h-[200px] min-w-full">
+      <img :src="image" class="absolute w-full h-full" alt="" />
+      <figcaption
+        class="absolute left-0 right-0 bottom-0 bg-violet-600 text-center text-xs text-white antialiased font-semibold pb-4 pt-2">
+        <label for="imageUser" class="cursor-pointer">Change Image</label>
+        <input type="file" name="use_img" id="imageUser" @change="changeImage" class="hidden" />
+      </figcaption>
+    </figure>
   </div>
 </template>
 
 <script>
 export default {
-  name: "AtomInputImage"
+  name: "AtomInputImage",
+  data() {
+    return {
+      image: 'https://cutewallpaper.org/24/image-placeholder-png/person-image-placeholder-clipart-png-download-no-profile-photo-vector-transparent-png-vhv.png'
+
+    }
+  },
+  methods: {
+    changeImage(e) {
+      const file = e.target.files[0]
+      this.image = URL.createObjectURL(file)
+
+      console.log(file)
+    }
+  },
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
